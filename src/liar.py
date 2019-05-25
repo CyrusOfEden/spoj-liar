27be80adc21887392b69073e4a7cdfbc1ba9e7e5
Kash Nouroozi <knouroozi@gmail.com>
2019-05-25 13:07:30 -0400

    :)

--- src/liar.py
+++ src/liar.py
@@ -1,90 +1,97 @@
 from __future__ import print_function
 
+try:
+    xrange = range
+except NameError:
+    pass
+
 
 def load_classes(from_file):
     test_cases = int(next(from_file))
     classes = [None] * test_cases
     for i in range(test_cases):
         count = int(next(from_file))
-        classes[i] = [[c == 'T' for c in next(from_file)]
+        classes[i] = [[c == 'T' for c in next(from_file).strip()]
                       for _ in range(count)]
     return classes
 
 
-def conclusion(replies, count, starting_with):
-    i, i_is_honest = starting_with
-    state = [None] * count
-    state[i] = i_is_honest
-    # Concluding what we can
-    for j in range(count):
-        if i == j:
-            continue
-        j_is_honest = state[j]
-        j_as_per_i = replies[i][j]
-        i_as_per_j = replies[j][i]
-        if i_as_per_j == j_as_per_i:
-            state[j] = i_is_honest
-        if i_is_honest:
-            if j_as_per_i is True and i_as_per_j is False:
-                return None, False
-            state[j] = j_as_per_i
-        elif replies[j][i]:
-            state[j] = False
-    # Here we're deciding whether each student was honest, or lied
-    for i, i_is_honest in enumerate(state):
-        i_lied = False
-        conclusive = False
+def initialize_state(survey, i, starting_value):
+    state = [None] * len(survey)
+    state[i] = starting_value
+    return state
+
+
+def conclusion(survey, state, as_per):
+    count = len(survey)
+    i = as_per
+    i_is_honest = state[i]
+    if i_is_honest:
+        if survey[i][i] is False:
+            return False, "Perjury"
         for j in range(count):
             if i == j:
                 continue
-            j_is_honest = state[j]
-            j_as_per_i = replies[i][j]
-            i_as_per_j = replies[j][i]
-            if j_as_per_i or i_as_per_j:
-                conclusive = True
-            if j_is_honest is not None and j_as_per_i != j_is_honest:
-                i_lied = True
-                break
-        if conclusive:
-            if i_is_honest is None:
-                state[i] = not i_lied
-            elif i_is_honest == i_lied:
-                return None, False
-    return state, True
-
-
-def class_score(replies):
-    count = len(replies)
-    min_liars = count
-    max_liars = -1
-
-    for i in range(count):
-        for assumption in (True, False):
-            state, ok = conclusion(replies,
-                                   count,
-                                   starting_with=(i, assumption))
-            if not ok:
+            j_as_per_i = survey[i][j]
+            i_as_per_j = survey[j][i]
+            if j_as_per_i is True and i_as_per_j is False:
+                return False, "Paradox"
+            state[j] = j_as_per_i
+    else:
+        for j in range(count):
+            if i == j:
                 continue
-            liar_count = sum(1 for v in state if not v)
-            if liar_count > max_liars:
-                max_liars = liar_count
-            elif liar_count < min_liars:
-                min_liars = liar_count
+            j_as_per_i = survey[i][j]
+            i_as_per_j = survey[j][i]
+            if i_as_per_j is True:
+                if state[j] is True:
+                    return False, "Paradox"
+                state[j] = False
+    if any(honesty is None for honesty in state):
+        for i, i_is_honest in enumerate(state):
+            any_lies = any(survey[i][j] != state[j] for j in range(count))
+            if i_is_honest is None:
+                state[i] = not any_lies
+            elif i_is_honest == any_lies:
+                return False, "Perjury"
+    for i, i_is_honest in enumerate(state):
+        any_lies = any(survey[i][j] != state[j] for j in range(count))
+        if i_is_honest == any_lies:
+            return False, "Perjury"
+    return True, state
 
-    if max_liars == -1:
-        return None, False
 
-    return (min_liars, max_liars), True
+def class_score(survey):
+    states = []
+
+    for i in range(len(survey)):
+        state = initialize_state(survey, i, True)
+        ok, state = conclusion(survey, state, as_per=i)
+        if ok:
+            states.append(state)
+        state = initialize_state(survey, i, False)
+        ok, state = conclusion(survey, state, as_per=i)
+        if ok:
+            states.append(state)
+
+    def liar_count(state):
+        return sum(1 for r in state if not r)
+
+    if not states:
+        return False, None
+
+    scores = [liar_count(s) for s in states]
+    return True, (min(scores), max(scores))
 
 
 def print_class_score(index, class_survey):
-    score, ok = class_score(class_survey)
-    if not ok:
-        print("Class Room#%s is paradoxical" % index)
-    else:
+    ok, score = class_score(class_survey)
+    if ok:
         atleast, atmost = score
         print("Class Room#%s contains atleast %d and atmost %d liars" %
               (index, atleast, atmost))
+    else:
+        print("Class Room#%s is paradoxical" % index)
 
 
 def run(in_file):
